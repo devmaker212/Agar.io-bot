b2290ef26ae6ab49b37f9878841d9d8e2c154fa5
devmaker212 <nickyriehl@gmail.com>
2015-06-14 19:59:05 -0400

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "YourMom",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
